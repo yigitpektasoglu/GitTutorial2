ceb29ba23c2686fb8f16a9d06f68d75f6a8777d3
Yigit P <ypektasoglu@gmail.com>
2022-08-24 16:13:29 +0300

    Initial Commit

---
+++ GitTutorial2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTutorial2
+//
+//  Created by Yiğit Pektaşoğlu on 24.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
